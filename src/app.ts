846730d23e1a5e9b567d3e32b46b415a286c4b64
Fabio Alejandro Quintero <fabio.quintero@gmail.com>
2024-04-15 11:23:45 -0500

    validando vigencia del partido

--- src/app.ts
+++ src/app.ts
@@ -174,10 +174,10 @@ bot.action(/^partido_(\d+)$/, async (ctx) => {
 
 bot.action(/^registrarse_(\d+)_(\d+)$/, async (ctx: any) => {
 
-	const currentDate = new Date();
+	//const currentDate = new Date();
 	const jugadorIdTelegram = ctx.match[1];
 	const partidoId = ctx.match[2];
-	const {fecha} = (await axios.get(`${envs.URL_API}/partido/${partidoId}`)).data;
+	//const {fecha} = (await axios.get(`${envs.URL_API}/partido/${partidoId}`)).data;
 	const listadoJugadores = (await axios.get(`${envs.URL_API}/partido_jugadores/partidojugadores_idpartido/${partidoId}`)).data;
 	const { id, nombre_corto } = (await axios.get(`${envs.URL_API}/jugadores/jugadoridteletram/"${jugadorIdTelegram}"`)).data;
 
@@ -185,7 +185,6 @@ bot.action(/^registrarse_(\d+)_(\d+)$/, async (ctx: any) => {
 		await axios.get(`${envs.URL_API}/partido_jugadores/partidojugadore_idjugador_idpartido/${id}/${partidoId}`)
 		await ctx.reply("ya esta registrado");
 	} catch (error) {
-		// if (new Date(fecha) > currentDate) {
 			await axios.post(`${envs.URL_API}/partido_jugadores/create_idjugador_idpartido`, {
 				"id_jugador": parseInt(id),
 				"id_partido": parseInt(partidoId),
@@ -194,10 +193,6 @@ bot.action(/^registrarse_(\d+)_(\d+)$/, async (ctx: any) => {
 			(listadoJugadores.length < 27)
 				? await ctx.reply("Ha sido registrado con exito")
 				: await ctx.reply(`Ha sido registrado en *reserva*, Ya que estan registrados ${listadoJugadores.length} Jugadores`);
-		// }
-		// else
-		// 	ctx.reply(`El partido que hace referencia ya paso por favor digita /start para buscar un partido vigente`)
-
 	}
 
 	// Notificar al usuario con ID 646386747 sobre el nuevo registro
